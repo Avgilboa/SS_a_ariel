3943ffa0a6bdde4314138f4cf40ea29a3cac377e
Avgilboa <35407628+Avgilboa@users.noreply.github.com>
2021-11-02 08:06:18 +0200

    Add files via upload

---
+++ basicClassification.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+int isPrime (int n);
+int isStrong (int n);
+
+
+int main()
+{
+    int num = 0;
+    scanf("%d", &num);
+    printf("%d" ,isPrime(num));
+}
+
+int isPrime(int num)
+{
+    for (int i=2; i<=(num/2) ; i++)
+    {
+        if(num%i==0)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int isStrong(int num)
+{
+    int sum=0;
+    int temp = 1;
+    int num1= num;
+    do{
+    for(int i=1; i<=num%10;i++)
+    {
+        temp*=i;
+    }
+    num = num/10;
+    sum +=temp;
+    temp =1;
+    } while(num!=0);
+    if(sum==num1)
+    {return 0;}
+    else{return 1;}
+}
